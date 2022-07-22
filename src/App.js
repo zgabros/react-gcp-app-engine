03ec81f2e0bbe567ac7db2cbbb06fae79e481b4c
Gabriel <zgabros@gmail.com>
2022-07-22 01:36:40 -0300

    segundo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Esto es un test de GCP</p>
+        <p>Esto es un test de GCP, mis amigos</p>
         <a
           className="App-link"
           href="https://reactjs.org"
